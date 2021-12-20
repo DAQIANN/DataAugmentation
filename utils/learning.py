f1eb2840c881c9048d077d1ec616a6462fc3425b
Akhil Kodumuri <akhilkodumuri@Akhils-MacBook-Pro.local>
2021-12-20 00:25:49 -0600

    finished annotating practice

--- utils/learning.py
+++ utils/learning.py
@@ -243,7 +243,65 @@ def imageRotationAndTranslation():
     cv2.imwrite('translated_image.jpg', translated_image)
     return "finished rotation and translation tutorial"
 
+def annotatingImages():
+    print("start annotating Images tutorial")
+    # Read Images
+    img = cv2.imread('sample.jpg')
+    # Display Image
+    cv2.imshow('Original Image',img)
+    cv2.waitKey(1)
+    # Print error message if image is null
+    if img is None:
+        print('Could not read image')
+    # Draw line on image
+    imageLine = img.copy()
+    # Draw the image from point A to B
+    pointA = (200,80)
+    pointB = (450,80)
+    cv2.line(imageLine, pointA, pointB, (255, 255, 0), thickness=3, lineType=cv2.LINE_AA)
+    cv2.imshow('Image Line', imageLine)
+    cv2.waitKey(1)
 
+    print("creating a circle annotation")
+    # Make a copy of image
+    imageCircle = img.copy()
+    # define the center of circle
+    circle_center = (415,190)
+    # define the radius of the circle
+    radius =100
+    #  Draw a circle using the circle() Function
+    # thickness=-1 fills the circle
+    cv2.circle(imageCircle, circle_center, radius, (0, 0, 255), thickness=3, lineType=cv2.LINE_AA) 
+    # Display the result
+    cv2.imshow("Image Circle",imageCircle)
+    cv2.waitKey(1)
+
+    # make a copy of the original image
+    imageRectangle = img.copy()
+    # define the starting and end points of the rectangle
+    start_point =(300,115)
+    end_point =(475,225)
+    # draw the rectangle
+    cv2.rectangle(imageRectangle, start_point, end_point, (0, 0, 255), thickness= 3, lineType=cv2.LINE_8) 
+    # display the output
+    cv2.imshow('imageRectangle', imageRectangle)
+    cv2.waitKey(1)
+    print("adding text to the picture")
+
+    # make a copy of the original image
+    imageText = img.copy()
+    #let's write the text you want to put on the image
+    text = 'I am a Happy dog!'
+    #org: Where you want to put the text
+    org = (50,350)
+    # write the text on the input image
+    cv2.putText(imageText, text, org, fontFace = cv2.FONT_HERSHEY_COMPLEX, fontScale = 1.5, color = (250,225,100))))
+    # display the output image with text over it
+    cv2.imshow("Image Text",imageText)
+    cv2.waitKey(1)
+    cv2.destroyAllWindows()
+
+    return "finished annotating image tutorial"
 
 if __name__ == "__main__":
     cropping()
